9df40d4d852257d454f049e624830f0369ee7e09
jecsh <mohamya77@gmail.com>
2023-12-26 00:49:54 +0200

    small changes

--- src/components/hero/Heroo.jsx
+++ src/components/hero/Heroo.jsx
@@ -5,7 +5,8 @@ const Heroo = () => {
   return (
     <div className="herocont">
         <video src="/video/v1.mp4" autoPlay loop muted />
-        <h1>project al shbab</h1>
+        <h1>OLIVE</h1>
+        <h2>CRUSHING MACHINE</h2>
         <p>what are you waiting for</p>
     <ScrollLink to="gg" smooth={true} duration={500}>
     <button className="btnn" >TAKE A LOOK</button>
