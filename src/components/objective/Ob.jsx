b23c18354ffc3bad3ffa35028e5c6816ba38a0d6
jecsh <mohamya77@gmail.com>
2024-01-19 10:28:00 +0200

    ss

--- src/components/objective/Ob.jsx
+++ src/components/objective/Ob.jsx
@@ -8,28 +8,28 @@ const Ob = () => {
       <h1 className="objectives-title">Objectives</h1>
       <div className="card-container">
         <div className="card">
-          <div className="card-content">
+          <div className="card-contenttt">
             <h2>Flexible Solution for Solar PV</h2>
             <p>This energy tracker  is designed to be a comprehensive solution for measuring Solar PV Applications, providing flexibility for both DC and AC power flow.
 </p>
           </div>
         </div>
         <div className="card">
-          <div className="card-content">
+          <div className="card-contenttt">
             <h2>Online Remote Monitoring</h2>
             <p>Through the Blynk app, we can monitor these parameters in real-time from anywhere, making it convenient for online remote monitoring.
 </p>
           </div>
         </div>
         <div className="card">
-          <div className="card-content">
+          <div className="card-contenttt">
             <h2>Test System Performance</h2>
             <p>Conduct comprehensive testing and validation of the Two-Way Meter system and the mobile app to ensure accuracy, reliability, and seamless functionality under various operational scenarios.
 </p>
           </div>
         </div>
         <div className="card">
-          <div className="card-content">
+          <div className="card-contenttt">
             <h2>Seek User Feedback</h2>
             <p>Encourage user feedback through pilot implementations, and utilize this feedback to make continuous improvements to both the hardware and software components, enhancing the overall user experience and system performance.
 </p>
