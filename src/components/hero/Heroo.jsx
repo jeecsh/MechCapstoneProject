09611e2da7ad84b8a3a72d9455952a5a3b68157a
jecsh <mohamya77@gmail.com>
2024-01-22 20:25:03 +0200

    bye

--- src/components/hero/Heroo.jsx
+++ src/components/hero/Heroo.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { Link as ScrollLink } from 'react-scroll';
-import './hero.css'
+import './hero.css';
+import videoBackground from "/Users/jecsh/capston mech/react/public/video/vb.mp4";
+
 const Heroo = () => {
   return (
     <div className="herocont">
-        <video src="/video/vb.mp4" autoPlay loop muted />
-        <h1>BiPowerTracker</h1>
-        <h2>Graduation project</h2>
-        <p>SO WHAT ARE YOU WAITING FOR !</p>
-    <ScrollLink to="gg" smooth={true} duration={500}>
-    <button className="btnn" >TAKE A LOOK</button>
-    </ScrollLink>
+      <video autoPlay loop muted>
+        <source src={videoBackground} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <h1>BiPowerTracker</h1>
+      <h2>Graduation project</h2>
+      <p>SO WHAT ARE YOU WAITING FOR !</p>
+      <ScrollLink to="gg" smooth={true} duration={500}>
+        <button className="btnn">TAKE A LOOK</button>
+      </ScrollLink>
     </div>
-  )
+  );
 }
 
-export default Heroo
+export default Heroo;
